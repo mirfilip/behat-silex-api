c6a4283d277da07fee12ab1d7d434ed9f1676aff
Mirosław Filip <miroslaw.filip@schibsted.pl>
2016-10-19 09:45:58 +0200

    Configure WebApiExtension and create a sample context

---
+++ tests/features/rest.feature
@@ -0,0 +1,6 @@
+Feature: RESTful paradigm check
+  In order to interact with RESTful API
+  As a user-agent
+  The service needs to respond in a RESTful manner
+
+  We interact with the allegedly RESTful API with a domain object: notes
---
+++ tests/features/src/Context/ApiContext.php
@@ -0,0 +1,15 @@
+<?php
+namespace Behat\Context;
+
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Behat\WebApiExtension\Context\WebApiContext;
+
+/**
+ * Defines application features from the specific context.
+ */
+class ApiContext extends WebApiContext
+{
+}
